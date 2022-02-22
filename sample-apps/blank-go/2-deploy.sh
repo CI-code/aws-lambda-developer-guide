fde7d071079ed3eb9de6ef937c194421a7291285
CI-code <reshmram@amazon.com>
2022-02-22 13:38:35 -0500

    Update 2-deploy.sh

--- sample-apps/blank-go/2-deploy.sh
+++ sample-apps/blank-go/2-deploy.sh
@@ -4,5 +4,6 @@ ARTIFACT_BUCKET=$(cat bucket-name.txt)
 cd function
 GOOS=linux go build main.go
 cd ../
+echo "Hello AWS"
 aws cloudformation package --template-file template.yml --s3-bucket $ARTIFACT_BUCKET --output-template-file out.yml
 aws cloudformation deploy --template-file out.yml --stack-name blank-go --capabilities CAPABILITY_NAMED_IAM
